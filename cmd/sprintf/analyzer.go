6e4cf60418adb1d154379ba1efe4fe9010462c27
Marco Siebecke <marco@siebecke.se>
2026-03-24 15:07:32 +0100

    PreorderSeq

--- cmd/sprintf/analyzer.go
+++ cmd/sprintf/analyzer.go
@@ -9,21 +9,44 @@ import (
 	"strings"
 
 	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
 )
 
 var Analyzer = &analysis.Analyzer{
-	Name: "printffinder",
-	Doc:  "checks for pure string concatenation in Sprintf",
-	Run:  run,
+	Name:     "printffinder",
+	Doc:      "checks for pure string concatenation in Sprintf",
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+	Run:      run,
 }
 
 func run(pass *analysis.Pass) (any, error) {
-	for _, astFile := range pass.Files {
-		ast.Inspect(astFile, func(n ast.Node) bool {
-			if n == nil {
-				return true
-			}
 
+	ins := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	// nodeFilter := []ast.Node{
+	// 	(*ast.BinaryExpr)(nil),
+	// 	(*ast.FuncDecl)(nil),
+	// 	(*ast.CallExpr)(nil),
+	// 	(*ast.SelectorExpr)(nil),
+	// }
+
+	//ins.Preorder(nodeFilter, checkSprintfConcat(pass))
+
+	for call := range ins.PreorderSeq((*ast.CallExpr)(nil)) {
+		checkSprintf(pass, call)
+	}
+	return nil, nil
+}
+
+func checkSprintfConcat(pass *analysis.Pass) func(n ast.Node) {
+	return func(n ast.Node) {
+		fn, ok := n.(*ast.FuncDecl)
+		if !ok || fn.Body == nil {
+			return
+		}
+
+		ast.Inspect(fn.Body, func(node ast.Node) bool {
 			callExpr, ok := n.(*ast.CallExpr)
 			if !ok {
 				return true
@@ -61,12 +84,10 @@ func run(pass *analysis.Pass) (any, error) {
 			}
 
 			sCount := strings.Count(strValue, "%s")
-			// Guard: Ensure there is at least one "%s" AND the string contains nothing else
 			if sCount == 0 || len(strValue) != sCount*2 {
 				return true
 			}
 
-			// Not enough parameters for all the %s another analyzer should fetch this
 			if len(callExpr.Args) < sCount+1 {
 				return true
 			}
@@ -82,13 +103,12 @@ func run(pass *analysis.Pass) (any, error) {
 			}
 			newText := strings.Join(args, " + ")
 
-			// If we survived the gauntlet of guards above, we have our match!
 			pass.Report(analysis.Diagnostic{
 				Pos:     ident.Pos(),
 				Message: "unnecessary use of fmt.Sprintf",
 				SuggestedFixes: []analysis.SuggestedFix{
 					{
-						Message: "replace with string concatenation",
+						Message: "replace with + operator string concatenation",
 						TextEdits: []analysis.TextEdit{
 							{
 								Pos:     ident.Pos(),
@@ -103,5 +123,88 @@ func run(pass *analysis.Pass) (any, error) {
 			return true
 		})
 	}
-	return nil, nil
+}
+
+func checkSprintf(pass *analysis.Pass, n ast.Node) {
+	fn, ok := n.(*ast.FuncDecl)
+	if !ok || fn.Body == nil {
+		return
+	}
+
+	ast.Inspect(fn.Body, func(node ast.Node) bool {
+		callExpr, ok := n.(*ast.CallExpr)
+		if !ok {
+			return true
+		}
+
+		selExpr, ok := callExpr.Fun.(*ast.SelectorExpr)
+		if !ok {
+			return true
+		}
+
+		ident := selExpr.Sel
+		info := pass.TypesInfo.Uses[ident]
+		if info == nil {
+			return true
+		}
+
+		pkg := info.Pkg()
+		if pkg == nil {
+			return true
+		}
+
+		if pkg.Path() != "fmt" || info.Name() != "Sprintf" {
+			return true
+		}
+
+		formatString := callExpr.Args[0]
+		lit, ok := formatString.(*ast.BasicLit)
+		if !ok || lit.Kind != token.STRING {
+			return true
+		}
+
+		strValue, err := strconv.Unquote(lit.Value)
+		if err != nil {
+			return true
+		}
+
+		sCount := strings.Count(strValue, "%s")
+		if sCount == 0 || len(strValue) != sCount*2 {
+			return true
+		}
+
+		if len(callExpr.Args) < sCount+1 {
+			return true
+		}
+
+		var args []string
+		for _, node := range callExpr.Args[1 : sCount+1] {
+			var buf bytes.Buffer
+			err := printer.Fprint(&buf, pass.Fset, node)
+			if err != nil {
+				return true
+			}
+			args = append(args, buf.String())
+		}
+		newText := strings.Join(args, " + ")
+
+		pass.Report(analysis.Diagnostic{
+			Pos:     ident.Pos(),
+			Message: "unnecessary use of fmt.Sprintf",
+			SuggestedFixes: []analysis.SuggestedFix{
+				{
+					Message: "replace with + operator string concatenation",
+					TextEdits: []analysis.TextEdit{
+						{
+							Pos:     ident.Pos(),
+							End:     ident.End(),
+							NewText: []byte(newText),
+						},
+					},
+				},
+			},
+		})
+
+		return true
+	})
 }
--- cmd/sprintf/analyzer_test.go
+++ cmd/sprintf/analyzer_test.go
@@ -4,17 +4,11 @@ import (
 	"testing"
 
 	"golang.org/x/tools/go/analysis/analysistest"
-	// Import your analyzer package
 )
 
 func TestAnalyzer(t *testing.T) {
 	// analysistest.TestData() figures out the absolute path to your 'testdata' folder
 	testdata := analysistest.TestData()
 
-	// analysistest.Run takes:
-	// 1. The testing object
-	// 2. The path to the testdata directory
-	// 3. Your Analyzer struct
-	// 4. The name of the fake package inside testdata/src/ to analyze (we used "a")
 	analysistest.Run(t, testdata, Analyzer, "mypkg")
 }
